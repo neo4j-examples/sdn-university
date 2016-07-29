a2d6cda2e4d548684f6e7e3ea791d4992c50e195
jblues <jasper@appsquick.ly>
2016-07-29 15:07:57 +0800

    Build against recent dependency versions.

--- src/main/java/school/Application.java
+++ src/main/java/school/Application.java
@@ -10,18 +10,81 @@
  */
 package school;
 
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.neo4j.NodeEntityScan;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.event.AfterDeleteEvent;
+import org.springframework.data.neo4j.event.AfterSaveEvent;
+import org.springframework.data.neo4j.event.BeforeSaveEvent;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@NodeEntityScan(basePackages = "school.domain")
-public class Application {
+@EnableNeo4jRepositories(basePackages = "school.repository")
+public class Application extends Neo4jConfiguration {
 
     public static void main(String[] args) {
         new SpringApplication(Application.class).run(args);
+    }
+
+    /**
+     * Creates a neo4j configuration, falling back to embedded if config details not present
+     */
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config.driverConfiguration().setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver");
+        config.driverConfiguration().setURI("http://neo4j:h4ckM3@localhost:7474");
+        return config;
+    }
+
+
+    @Bean
+    public SessionFactory getSessionFactory() {
+        return new SessionFactory(getConfiguration(), "school.domain");
+    }
+
+    public Session getSession() throws Exception {
+        return super.getSession();
+    }
+
+
+    @Bean
+    ApplicationListener<BeforeSaveEvent> beforeSaveEventApplicationListener() {
+        return new ApplicationListener<BeforeSaveEvent>() {
+            @Override
+            public void onApplicationEvent(BeforeSaveEvent event) {
+                Object entity = event.getEntity();
+                System.out.println("Before save of: " + entity);
+            }
+        };
+    }
+
+    @Bean
+    ApplicationListener<AfterSaveEvent> afterSaveEventApplicationListener() {
+        return new ApplicationListener<AfterSaveEvent>() {
+            @Override
+            public void onApplicationEvent(AfterSaveEvent event) {
+                Object entity = event.getEntity();
+                System.out.println("Before save of: " + entity);
+            }
+        };
+    }
+
+    @Bean
+    ApplicationListener<AfterDeleteEvent> deleteEventApplicationListener() {
+        return new ApplicationListener<AfterDeleteEvent>() {
+            @Override
+            public void onApplicationEvent(AfterDeleteEvent event) {
+                Object entity = event.getEntity();
+                System.out.println("Before save of: " + entity);
+            }
+        };
     }
 
 }
