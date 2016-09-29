b6c6326c7fc68ecfee9eb740ea742fdde99804a3
Mark Angrish <mark.angrish@gmail.com>
2016-09-29 18:14:29 +1000

    Fixed issue with driver being null

--- src/main/java/school/Neo4jConfiguration.java
+++ src/main/java/school/Neo4jConfiguration.java
@@ -1,6 +1,9 @@
 package school;
 
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +20,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableTransactionManagement
 @ComponentScan({"school.service"})
 @EnableWebMvc
+@EnableConfigurationProperties(Neo4jProperties.class)
 public class Neo4jConfiguration extends WebMvcConfigurerAdapter {
+
+	private final Neo4jProperties properties;
+
+	public Neo4jConfiguration(Neo4jProperties properties) {
+		this.properties = properties;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public org.neo4j.ogm.config.Configuration configuration() {
+		return this.properties.createConfiguration();
+	}
 
 	@Bean
 	public OpenSessionInViewInterceptor openSessionInViewInterceptor() {
 		OpenSessionInViewInterceptor openSessionInViewInterceptor =
 				new OpenSessionInViewInterceptor();
-		openSessionInViewInterceptor.setSessionFactory(sessionFactory());
+		openSessionInViewInterceptor.setSessionFactory(sessionFactory(configuration()));
 		return openSessionInViewInterceptor;
 	}
 
@@ -33,13 +49,13 @@ public class Neo4jConfiguration extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	public SessionFactory sessionFactory() {
-		return new SessionFactory("school.domain");
+	public SessionFactory sessionFactory(org.neo4j.ogm.config.Configuration configuration) {
+		return new SessionFactory(configuration, "school.domain");
 	}
 
 	@Bean
-	public Neo4jTransactionManager transactionManager() {
-		return new Neo4jTransactionManager(sessionFactory());
+	public Neo4jTransactionManager transactionManager(SessionFactory sessionFactory) {
+		return new Neo4jTransactionManager(sessionFactory);
 	}
 
 }
